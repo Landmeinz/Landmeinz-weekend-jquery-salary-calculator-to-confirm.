9d23d6b9bb746a811e9e0aaff9f9a1994b95451a
Eric Meinzer <emeinzer7@gmail.com>
2021-10-01 18:33:42 -0500

    render to DOM / emptyInput setup

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 // console.log('js connected');
 $(handleStart);
 
+let employeeList = [];
+
 // Create an application that records employee salaries and adds salaries up to report monthly costs. 
 function handleStart(){
     // console.log('jq connected');
     // when the inputs are filled; push to table when the submitButton is clicked;
-    $(`#submitButton`).on(`click`, addEmployee)
+    $(`#submitButton`).on(`click`, addEmployee);
 }
 
+
 function addEmployee(){
-    console.log('employee added');
+    console.log('submit button clicked');
  
     // create and object with keys to store our table data;
     const tableData = {
@@ -19,7 +22,7 @@ function addEmployee(){
         title: $(`#inputTitle`).val(),
         salary: $(`#inputSalary`).val(),
     }
-    console.log(tableData);
+    // console.log(tableData);
 
     // dont allow the user to add empty info; all fields must be completed;
     if(tableData.firstName === '' || 
@@ -30,12 +33,46 @@ function addEmployee(){
         alert(`Add all info to submit`);
     }
     
-    // push data onto the DOM for each submit;
+    // lets push data into our employeeList array;
+    employeeList.push(tableData)
+    // console.log(employeeList);
     
-
-    // empty input fields;
-
-
+    renderDOM();
+    
 }
+
+
+// lets empty all inputs with one function; call before we render;
+function emptyInputs(){
+    $(`#inputFirstName`).empty();
+    $(`#inputLastName`).empty();
+    $(`#inputNumber`).empty();
+    $(`#inputTitle`).empty();
+    $(`#inputSalary`).empty();
+}
+
+
+function renderDOM(){
+    console.log(employeeList);
+    emptyInputs();
+
+     for(let person of employeeList){
+        const tableRowData = $(`
+        <tr>
+            <td>${person.firstName}</td>
+            <td>${person.lastName}</td>
+            <td>${person.number}</td>
+            <td>${person.title}</td>
+            <td>${person.salary}</td>
+        </tr>
+        `);
+
+        $(`#displayEmployee`).append(tableRowData);
+    }
+}
+
+
+
+
 
 
