a1cec19b0f59d5dee7af2360ccdaef2e3005b12d
Eric Meinzer <emeinzer7@gmail.com>
2021-10-01 17:37:26 -0500

    table data getter functionally

--- app.js
+++ app.js
@@ -10,7 +10,32 @@ function handleStart(){
 
 function addEmployee(){
     console.log('employee added');
+ 
+    // create and object with keys to store our table data;
+    const tableData = {
+        firstName: $(`#inputFirstName`).val(),
+        lastName: $(`#inputLastName`).val(),
+        number: $(`#inputNumber`).val(),
+        title: $(`#inputTitle`).val(),
+        salary: $(`#inputSalary`).val(),
+    }
+    console.log(tableData);
+
+    // dont allow the user to add empty info; all fields must be completed;
+    if(tableData.firstName === '' || 
+        tableData.lastName === '' ||
+        tableData.number === '' ||
+        tableData.title === '' ||
+        tableData.salary === '' ){
+        alert(`Add all info to submit`);
+    }
     
+    // push data onto the DOM for each submit;
+    
+
+    // empty input fields;
+
+
 }
 
 
