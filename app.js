0328f806ec9466a59987be211ee56dada2f10db5
Eric Meinzer <emeinzer7@gmail.com>
2021-10-02 15:37:03 -0500

    new js file

--- app.js
+++ app.js
@@ -2,8 +2,82 @@
 $(handleStart);
 
 let employeeList = [];
+let runningTotal = 0;
 
-let globalEmployeeList = [];
+
+// function to DISPLAY all of the persons info to the DOM; 
+function renderDOM(){
+    // dump any old inputs before we loop;
+    emptyInputs();
+    // dump out the array before we loop again to grab more info; prevents the double display issue; 
+    employeeList = [];
+
+    // accessing the people that make up the array of employees; 
+    for(let person of employeeList){
+        // actual data to be pushed into our html <tbody id="displayEmployee">;
+        const tableRowData = $(`
+        <tr>
+            <td>${person.firstName}</td>
+            <td>${person.lastName}</td>
+            <td>${person.number}</td>
+            <td>${person.title}</td>
+            <td>${numberWithCommas(person.salary)}</td>
+            <td> <button class="remove-button">REMOVE</button> </td>
+        </tr>
+        `);
+
+        // adding each person to the employeeList;
+        employeeList.push(person);
+        console.log(employeeList);
+
+        // table row data displaying on the DOM in the <tbody> 
+        $(`#displayEmployee`).append(tableRowData);
+
+        calcTotal();
+    } 
+}
+
+
+
+// lets EMPTY all inputs with one function; called below just before we render;
+function emptyInputs(){
+    $(`#inputFirstName`).val('');
+    $(`#inputLastName`).val('');
+    $(`#inputNumber`).val('');
+    $(`#inputTitle`).val('');
+    $(`#inputSalary`).val('');
+}
+
+
+
+// function to calc our MONTHLY; this should alter the employeeList; 
+function calcTotal(){
+
+    // get the employee annual salary; we want a monthly expense so lets divide by 12;
+    let salary = employeeList.salary;
+    console.log(salary);
+    salary = parseInt(salary / 12);
+
+    // target the html <span> input value;
+    let totalMonthlyDisplay = $('#totalMonthlyDisplay');
+    // target the html <h3> text; 
+
+    let totalMonthlyText = $(`#totalMonthlyText`);
+    // add the persons salary to the running total; 
+    runningTotal += Number(salary);
+    runningTotal = parseInt(runningTotal); 
+    console.log('calculating total monthly:', runningTotal);
+
+    // change the text color to red on the DOM; both <h3> text and the displayed <span> text;
+    if(runningTotal > 20000){
+        totalMonthlyText.css(`color`, `red`);
+        totalMonthlyDisplay.css(`color`, `red`);
+    }
+    totalMonthlyDisplay.text(numberWithCommas(runningTotal));
+};
+
+
+
 
 
 
@@ -38,23 +112,10 @@ function addEmployee(){
         console.log(employeeList);
     }
     
-    
     // the big reveal; 
     renderDOM();
-
 }
 
-
-
-
-// lets EMPTY all inputs with one function; called below just before we render;
-function emptyInputs(){
-    $(`#inputFirstName`).val('');
-    $(`#inputLastName`).val('');
-    $(`#inputNumber`).val('');
-    $(`#inputTitle`).val('');
-    $(`#inputSalary`).val('');
-}
 
 
 // function to turn our large numbers into a sting that's something more readable; $24500 to $24,500;
@@ -63,84 +124,21 @@ function numberWithCommas(num) {
 }
 
 
-let runningTotal = 0;
-
-// function to DISPLAY all of the persons info to the DOM; 
-function renderDOM(){
-    // console.log(employeeList);
-    // dump any old inputs before we loop;
-    emptyInputs();
 
 
-    // accessing the people that make up the array of employees; 
-    for(let person of employeeList){
-        // actual data to be pushed into our html <tbody id="displayEmployee">;
-        const tableRowData = $(`
-        <tr>
-            <td>${person.firstName}</td>
-            <td>${person.lastName}</td>
-            <td>${person.number}</td>
-            <td>${person.title}</td>
-            <td>${numberWithCommas(person.salary)}</td>
-            <td> <button class="remove-button">REMOVE</button> </td>
-        </tr>
-        `);
-
-
-        // we want a monthly expense so lets divide by 12 here;
-        let salary = person.salary;
-        salary = parseInt(salary / 12);
-
-        // target the html <span> input value;
-        let totalMonthlyDisplay = $('#totalMonthlyDisplay');
-        // target the html <h3> text; 
-        let totalMonthlyText = $(`#totalMonthlyText`);
-
-        // function to calc our MONTHLY; update on the DOM; 
-        function calcTotal(){
-            // add the persons salary to the running total; 
-            runningTotal += Number(salary);
-            runningTotal = parseInt(runningTotal); 
-            console.log('calculating total monthly:', runningTotal);
-
-            // change the text color to red on the DOM; both <h3> text and the displayed <span> text;
-            if(runningTotal > 20000){
-                totalMonthlyText.css(`color`, `red`);
-                totalMonthlyDisplay.css(`color`, `red`);
-            }
-            totalMonthlyDisplay.text(numberWithCommas(runningTotal));
-        };
-
-        // adding each person to another array so we can use later; fixes the need to dump employeeList; 
-        globalEmployeeList.push(person);
-        console.log(employeeList);
-        // dump out the array before we loop again to grab more info; prevents the double display issue; 
-        employeeList = [];
-
-        // table row data displaying on the DOM in the <tbody> 
-        $(`#displayEmployee`).append(tableRowData);
-
-        calcTotal();
-        
-    } 
-
-    console.log(globalEmployeeList);
-    
-    
-}
 
 
 // when removed button is pushed decrease total;
 function removedTotal(){
-    console.log(globalEmployeeList);
+    console.log(employeeList);
     console.log(runningTotal);
     
     // target the html <span> input value;
     let totalMonthlyDisplay = $('#totalMonthlyDisplay');
     
     // use for loop to 
-    for(let person in globalEmployeeList){
-        runningTotal += globalEmployeeList[person].salary;
+    for(let person in employeeList){
+        runningTotal += employeeList[person].salary;
         console.log(person.salary);
         
     }
@@ -153,39 +151,23 @@ function removedTotal(){
 }
 
 
-// REMOVE the employee row of data from the DOM; log who was removed;
+// REMOVE the employee row of data from the DOM; log who was removed to a new array?
 function removeEmployee(){
-    console.log(`removed button clicked`);    
-
-    // // // // // --- let's log who go removed; --- // // // // // 
-    // let removedEmployee = $(this).val();
-    // console.log(removeEmployee);
-
-    // update total monthly display text; 
-    // removedTotal();
+    console.log(`(2) removed button clicked`);    
 
     console.log($(this).closest(`tr`).index());
     // targeting the row adjacent to the button; 
     $(this).closest(`tr`).remove();
    
-    
-    console.log(globalEmployeeList);
-    
-
+    console.log(employeeList);
 }
 
 
-
-
-
-// Create an application that records employee salaries and adds salaries up to report monthly costs. 
+// records employee salaries and adds salaries up to report monthly costs; 
 function handleStart(){
     // console.log('jq connected');
-
     // when the inputs are filled; push to table when the submitButton is clicked;
     $(`#submitButton`).on(`click`, addEmployee);
-
     // when we are done with this person remove them from the DOM when clicked; 
     $(`#displayEmployee`).on(`click`, `.remove-button`, removeEmployee);
-
 }
